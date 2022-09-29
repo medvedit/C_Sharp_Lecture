87e0ea1e4c38747a04fe57b5b39f584749095bef
medvedit <medwed.sa.job@gmail.com>
2022-09-30 01:20:55 +0300

    Example 006 Condition_If_Else

---
+++ Example006_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") // Оператор ToLower()выравнивает регистр введенного имяни всегда с маленькой буквы.
+{
+    Console.WriteLine("Ура, это же МАША!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
