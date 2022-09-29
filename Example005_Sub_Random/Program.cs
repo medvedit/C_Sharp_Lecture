3c1f9f3956c461a5a66d418a304f068bdc92fb19
medvedit <medwed.sa.job@gmail.com>
2022-09-30 00:34:48 +0300

    Example 005 Sun Random corrected//

--- Example005_Sub_Random/Program.cs
+++ Example005_Sub_Random/Program.cs
@@ -1,4 +1,4 @@
-﻿int namberA = new Random().Next(1, 10); //1 2 3 4 ... 10
+﻿int namberA = new Random().Next(1, 10); //1 2 3 4 ... 9
 Console.WriteLine(namberA);
 int namberB = new Random().Next(1, 10);
 Console.WriteLine(namberB);
