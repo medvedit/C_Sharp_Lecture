c7eaefd3d0c94ead7f5873953edabae7ffbe4d68
medvedit <medwed.sa.job@gmail.com>
2022-10-19 21:56:14 +0300

    метод рекурсия + фибоначчи

---
+++ Example016_fibonacci_factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿// Фибоначчи это:
+// f(1) = 1
+// f(2) = 1
+// f(3) = 2
+// f(4) = 3
+// f(5) = 5
+// f(6) = 8
+// ...
+// f(n) = f(n - 1) + f(n - 2)
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+ for (int i = 1; i < 50; i++)
+ {
+    Console.WriteLine($" {i}  {Fibonacci(i)}");
+ }
