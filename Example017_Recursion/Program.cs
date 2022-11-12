a43707a322288f96058c0bc81282921ad434c1f9
medvedit <medwed.sa.job@gmail.com>
2022-11-12 22:13:59 +0300

    Task 2,3

--- Example017_Recursion/Program.cs
+++ Example017_Recursion/Program.cs
@@ -13,6 +13,7 @@ string NumberFor(int a, int b)
 }
 Console.WriteLine(NumberFor(1, 10)); // 1 2 3 4 5 6 7 8 9 10
 Console.WriteLine();
+
 // Вариант через рекурсию
 string NumberRec(int a, int b)
 {
@@ -20,3 +21,38 @@ string NumberRec(int a, int b)
     else return String.Empty;
 }
 Console.WriteLine(NumberFor(1, 20)); // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+Console.WriteLine();
+
+//Задача №2 Сумма чисел от 1 до n
+// вариант с циклом for
+int SumFor(int N)
+{
+    int result = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+Console.WriteLine(SumFor(10)); // 55
+Console.WriteLine();
+// решение через рекурсию
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumRec(15)); // 120
+Console.WriteLine();
+
+// Задача№3 Три шпиля и count количетво блинов. Башенка.
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 6)
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+Towers();
+Console.WriteLine();
+
