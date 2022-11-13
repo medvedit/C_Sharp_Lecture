87cedc8e7ab67fd0219f7a646ca0661a52c85cdb
medvedit <medwed.sa.job@gmail.com>
2022-11-13 11:54:54 +0300

    добавил итеративный метод

--- Example016_fibonacci_factorial/Program.cs
+++ Example016_fibonacci_factorial/Program.cs
@@ -8,15 +8,15 @@
 // ...
 // f(n) = f(n - 1) + f(n - 2)
 
-double Fibonacci(int n)
-{
-    if(n == 1 || n == 2) return 1;
-    else return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
- for (int i = 1; i < 50; i++)
- {
-    Console.WriteLine($" {i}  {Fibonacci(i)}");
- }
+// double Fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+// for (int i = 1; i < 50; i++)
+// {
+//     Console.WriteLine($" {i}  {Fibonacci(i)}");
+// }
 //  1  1    Ответ:
 //  2  1
 //  3  2
@@ -66,3 +66,60 @@ double Fibonacci(int n)
 //  47  2971215073
 //  48  4807526976
 //  49  7778742049
+
+// Метод фибоначи по итеративному методу:
+
+decimal fIte = 0;
+
+decimal FibonacciIterration(int n)
+{
+    fIte++;
+    decimal result = 1;
+    decimal f0 = 1;
+    decimal f1 = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result = f0 + f1;
+        f0 = f1;
+        f1 = result;
+        fIte++;
+    }
+    return result;
+}
+Console.WriteLine();
+for (int n = 10; n <= 40; n++)
+{
+    Console.WriteLine($"FibonacciIterration({n}) = {FibonacciIterration(n)} fIte = {fIte}");
+    fIte = 0;
+}
+// FibonacciIterration(10) = 89 fIte = 10
+// FibonacciIterration(11) = 144 fIte = 11
+// FibonacciIterration(12) = 233 fIte = 12
+// FibonacciIterration(13) = 377 fIte = 13
+// FibonacciIterration(14) = 610 fIte = 14
+// FibonacciIterration(15) = 987 fIte = 15
+// FibonacciIterration(16) = 1597 fIte = 16
+// FibonacciIterration(17) = 2584 fIte = 17
+// FibonacciIterration(18) = 4181 fIte = 18
+// FibonacciIterration(19) = 6765 fIte = 19
+// FibonacciIterration(20) = 10946 fIte = 20
+// FibonacciIterration(21) = 17711 fIte = 21
+// FibonacciIterration(22) = 28657 fIte = 22
+// FibonacciIterration(23) = 46368 fIte = 23
+// FibonacciIterration(24) = 75025 fIte = 24
+// FibonacciIterration(25) = 121393 fIte = 25
+// FibonacciIterration(26) = 196418 fIte = 26
+// FibonacciIterration(27) = 317811 fIte = 27
+// FibonacciIterration(28) = 514229 fIte = 28
+// FibonacciIterration(29) = 832040 fIte = 29
+// FibonacciIterration(30) = 1346269 fIte = 30
+// FibonacciIterration(31) = 2178309 fIte = 31
+// FibonacciIterration(32) = 3524578 fIte = 32
+// FibonacciIterration(33) = 5702887 fIte = 33
+// FibonacciIterration(34) = 9227465 fIte = 34
+// FibonacciIterration(35) = 14930352 fIte = 35
+// FibonacciIterration(36) = 24157817 fIte = 36
+// FibonacciIterration(37) = 39088169 fIte = 37
+// FibonacciIterration(38) = 63245986 fIte = 38
+// FibonacciIterration(39) = 102334155 fIte = 39
+// FibonacciIterration(40) = 165580141 fIte = 40
